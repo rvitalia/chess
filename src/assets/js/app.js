e90c131a1c44352dff4cd111b5a2e938b15a6763
rvitalia <riabinines@yandex.ru>
2024-09-15 20:17:10 +0300

    desktop project

---
+++ src/assets/js/app.js
@@ -0,0 +1,1 @@
+new Swiper(".swiper_participants", { slidesPerView: 3, spaceBetween: 57, navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }, pagination: { el: ".swiper-pagination", type: "fraction", formatFractionCurrent: e =>  e, formatFractionTotal: e =>  e } })
